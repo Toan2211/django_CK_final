65fb3b830a9ef39716edb3aa72fab697dadce052
TUANDA-13 <duonganhtuan1314@gmail.com>
2022-06-21 01:38:04 +0700

    add

--- Blog/views.py
+++ Blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import redirect, render 
 from django.http import HttpResponse
-# Create your views here.
 from .models import *
 from django.contrib import auth
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from math import ceil
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def BASE(request):
     return render(request,'base.html')
@@ -16,7 +18,10 @@ def LOGOUT(request):
     auth.logout(request)
     return render(request,'login.html')
 
-@login_required(login_url='login')
+def GETUSER(request):
+    if request.user:
+        print(request.user)
+
 def INDEX(request):
     trending_post = Post.objects.filter(section ='Trending').order_by('date')[0:4]
     popular_post = Post.objects.filter(section ='Popular').order_by('date')[0:5]
@@ -26,14 +31,12 @@ def INDEX(request):
         'popular_post':popular_post,
         'topic':topic,
     }
+
     return render(request,'index.html',context)
 
-
 class Login(View):
-    @login_required(login_url='login')
     def get(self,request):
         return render(request,'login.html')
-
     def post(self,request):
         username = request.POST['username']
         password = request.POST['password']
@@ -42,19 +45,23 @@ class Login(View):
             auth.login(request,user) 
             print(1)
             
+            # request.set_cookie('user_name', username)
         else:
             # Return an 'invalid login' error message.
             print(username,password)
         return redirect('home')
 
 class Blog(View):
-    @login_required(login_url='login')
     def get(self,request,pk):
         p = Post.objects.get(pk=pk)
         print(p)
         context = {
             'data' : p
         }
+        print(1)
+        post = Post.objects.all()
+        for i in post:
+            print(i.author)
         return HttpResponse ('<h1>Xin chao<h1>',context)
     def post(self,request):
         pass
@@ -66,7 +73,11 @@ class Register(View):
         return render(request,'register.html')
     
     def post(self,request):
-        pass
+        user = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        passwordConf = request.POST.get('passwordConf')
+        print(user)
 
 class Single(View):
     def get(self,request,pk):
@@ -82,25 +93,29 @@ class Single(View):
     def post(self,request):
         pass
 
-class AddPost(View):
+class AddPost(LoginRequiredMixin,View):
+    login_url = '/login/'
     def get(self,request):
         return render(request,'addPost.html')
     def post(self,request):
         pass
-
-class ManageTopic(View):
+    
+class ManageTopic(LoginRequiredMixin,View):
+    login_url = '/login/'
     def get(self,request):
         return render(request,'managetopic.html')
     def post(self,request):
         pass
 
-class ManagePost(View):
+class ManagePost(LoginRequiredMixin,View):
+    login_url = '/login/'
     def get(self,request):
         return render(request,'managepost.html')
     def post(self,request):
         pass
 
-class AddTopic(View):
+class AddTopic(LoginRequiredMixin,View):
+    login_url = '/login/'
     def get(self,request):
         return render(request,'addTopic.html')
     def post(self,request):
@@ -111,24 +126,46 @@ class AddTopic(View):
             return HttpResponse('<h1>Loi<h1>')
         topic = Topic(name=title,description=description)
         topic.save()
+        messages.success("Thanh cong")
         return render(request,'addTopic.html')
         
 
-class ManageUser(View):
+class ManageUser(LoginRequiredMixin,View):
+    login_url = '/login/'
+    # @login_required(login_url='login')
     def get(self,request):
         return render(request,'manageuser.html')
     def post(self,request):
         
         return render(request,'addTopic.html')
         
-class AddUser(View):
+# @login_required(login_url='login')
+class AddUser(LoginRequiredMixin,View):
+    login_url = '/login/'
     def get(self,request):
         return render(request,'addUser.html')
     def post(self,request):
-        
-        return render(request,'addUser.html')
+        user = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        passwordConf = request.POST.get('passwordConf')
+        if password!=passwordConf:
+            messages.success(request, "Mật khẩu không khớp" )
+            return render(request,'addUser.html')
+        User = get_user_model()
+        users = User.objects.filter(username = user)
+        if users:
+            messages.success(request, "Tài khoản đã tồn tại" )
+            return render(request,'addUser.html')
+        else:
+            user = User.objects.create_user(user, email, password)
+            user.save()
+            GETUSER(request)
+            messages.success(request, "Thanh cong" )
+            return render(request,'addUser.html')
 
-class TopicView(View):
+class TopicView(LoginRequiredMixin,View):
+    login_url = '/login/'
     def get(self,request,pk,page=1):
         if type(pk) == int:
             tp = Topic.objects.filter(pk=pk).first()
@@ -161,8 +198,7 @@ class TopicView(View):
                 'next':next,
                 'page_list':page_list,
                 'topic':topic,
-            }
-            print(context)
+            }   
         return render(request,'topic.html',context)
     def post(self,request):
         return render(request,'addUser.html')
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -11,10 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+PROJECT_DIR = os.path.dirname(__file__)
+ROOT_PATH = os.path.dirname(__file__)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+def look_folder_tree(root):
+    result = ()
+    for dir_name, sub_dirs, file_names in os.walk(root):
+        for sub_dir_name in sub_dirs:
+            result += (os.path.join(dir_name, sub_dir_name),)
+    return result
+
+# Django settings for project.
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -118,14 +128,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = look_folder_tree(STATIC_ROOT)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 #Tu codee
 import os
 MEDIA_URL = '/media/' 
